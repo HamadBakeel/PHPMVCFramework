6d52c6fc707b94c6225d95bee54a3485d3652490
Hamad Bakeel <hamad200714@gmail.com>
2022-03-25 22:14:03 +0300

    First Commit

---
+++ Core/Router.php
@@ -0,0 +1,33 @@
+<?php
+namespace app\core;
+class Router
+{
+    protected array $routes = [];
+    public Request $request;
+
+    /**
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public  function  get($path,$callback){
+        $this->routes['get'][$path] = $callback;
+    }
+
+    public function resolve()
+    {
+        $path = $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path] ?? false;
+        if ($callback === false){
+            echo "<h1>Page Not Found !!</h1>";
+            exit();
+        }
+
+
+        echo call_user_func($callback);
+    }
+}
